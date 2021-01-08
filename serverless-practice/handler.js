7af82619d17cc680ceec43429469f88050cd08b2
elcorbs <emma@madetech.com>
2021-01-08 15:15:34 +0000

    deploy to serverless

--- serverless-practice/handler.js
+++ serverless-practice/handler.js
@@ -1,17 +1,7 @@
 'use strict';
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Ping pong!!! (serverless)',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+module.exports.ping = async (event) => {
+  return { body: JSON.stringify({message: "Pong" })};
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
